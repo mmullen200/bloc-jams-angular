9c4de36fb2550d0cede7bb0cb3367e1fbd9281c5
Michael Mullen <michaeldmullen@gmail.com>
2015-12-29 22:58:45 -0300

    assignment 2 complete

---
+++ app/scripts/app.js
@@ -0,0 +1,3 @@
+// The first argument, blocJams, is the prescribed name of the module. The empty array -- second argument -- injects dependencies into an application.
+
+angular.module('blocJams', []);
