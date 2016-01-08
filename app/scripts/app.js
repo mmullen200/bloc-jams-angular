d4f48c3d47e59d0fca09fd8bd5b7e038387b1023
Michael Mullen <michaeldmullen@gmail.com>
2016-01-08 12:45:36 -0800

    checkpoint 3 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,27 @@
-<<<<<<< HEAD
-=======
+
 // The first argument, blocJams, is the prescribed name of the module. The empty array -- second argument -- injects dependencies into an application.
 
->>>>>>> 894bc051daf8e61867c88c8a98afe93c6e5fb908
-angular.module('blocJams', []);
+(function() {
+     function config($stateProvider, $locationProvider) {
+         $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+         
+         $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         });
+     }
+ 
+ angular.module('blocJams', ['ui.router']);
+     angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+ })();
