a1086720f33781a9c41a1af87ce8e8fb5a2270d4
Michael Mullen <michaeldmullen@gmail.com>
2016-01-10 14:23:21 -0800

    ui-srefs changed back to hrefs in links on index page

---
+++ dist/scripts/app.js
@@ -0,0 +1,31 @@
+
+// The first argument, blocJams, is the prescribed name of the module. The empty array -- second argument -- injects dependencies into an application.
+
+(function() {
+     function config($stateProvider, $locationProvider) {
+         $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+         
+         $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         })
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
+         });
+     }
+ 
+ angular.module('blocJams', ['ui.router']);
+     angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+ })();
