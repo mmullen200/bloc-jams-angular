2a398a7fd8599ebd604fbc4f0a5da58618bdeef8
Michael Mullen <michaeldmullen@gmail.com>
2016-01-14 07:57:49 -0800

    removed stray semicolons from

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,11 +14,11 @@
              url: '/',
              controller: 'LandingCtrl as landing',
              templateUrl: '/templates/landing.html'
-         });
+         })
          .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
-         });
+         })
          .state('collection', {
              url: '/collection',
              controller: 'CollectionCtrl as collection',
